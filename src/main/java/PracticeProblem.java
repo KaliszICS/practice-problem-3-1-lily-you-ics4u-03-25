0fb0168d8c5758ca33cddb449a2453c839d62606
lily <lily.you@student.tdsb.on.ca>
2025-04-03 03:28:40 +0000

    completed 3.1

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,29 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static int sumOfDigits(int x){
+		if(x < 0){
+			x = x*-1;
+		}
 
+		if(0 <= x && x <= 9){
+			return x;
+		}
+
+		return sumOfDigits(x/10) + (x%10);
+		
+	}
+
+	public static int factorial(int n){
+		if(n == 0){
+			return 1;
+		}
+
+		if(n < 0){
+			return 0;
+		}
+
+		return n * factorial(n-1);
+		
+	}
 }
